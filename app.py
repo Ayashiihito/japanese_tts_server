c616fc2ea864daa077980511e7d652fa3c48adf6
Vladimir Antsupov <antsupov.vova@gmail.com>
2021-08-12 21:56:17 +0300

    tmp dir

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import time
 import torch
+import tempfile
 import soundfile as sf
 from espnet_model_zoo.downloader import ModelDownloader
 from espnet2.bin.tts_inference import Text2Speech
@@ -7,20 +8,19 @@ from parallel_wavegan.utils import download_pretrained_model
 from parallel_wavegan.utils import load_model
 from flask import Flask, send_file, request
 
-sample_rate = 24000
-lang = 'Japanese'
-model_tag = 'kan-bayashi/jsut_conformer_fastspeech2_accent_with_pause' 
-vocoder_tag = "jsut_parallel_wavegan.v1" 
+SAMPLE_RATE = 24000
+MODEL_TAG = 'kan-bayashi/jsut_conformer_fastspeech2_accent_with_pause' 
+VOCODER_TAG = "jsut_parallel_wavegan.v1" 
 
 downloader = ModelDownloader()
 
 text2speech = Text2Speech(
-    **downloader.download_and_unpack(model_tag),
-    device="cuda",
-    speed_control_alpha=1.0,
+    **downloader.download_and_unpack(MODEL_TAG),
+    device = "cuda",
+    speed_control_alpha = 1.0,
 )
 text2speech.spc2wav = None
-vocoder = load_model(download_pretrained_model(vocoder_tag)).to("cuda").eval()
+vocoder = load_model(download_pretrained_model(VOCODER_TAG)).to("cuda").eval()
 vocoder.remove_weight_norm()
 
 
@@ -41,8 +41,8 @@ def get_audio():
         torch.cuda.empty_cache() 
         return "Speech generation failed", 500
 
-    file_name = f"tmp/{start_time}_{text[:10]}.wav"
-    sf.write(file_name, wav.view(-1).cpu().numpy(), sample_rate, "PCM_16")
+    file_name = f"{tempfile.gettempdir()}/{start_time}_{text[:10]}.wav"
+    sf.write(file_name, wav.view(-1).cpu().numpy(), SAMPLE_RATE, "PCM_16")
 
     return send_file(file_name)
 
